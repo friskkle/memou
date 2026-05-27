a8af5f7e985aea24ab91c1ac988c74bb3d3f3c95
friskkle <matthewlurin127@gmail.com>
2026-05-27 23:31:23 +0800

    fix: back button sizing in login page no longer cramped

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,17 +1,19 @@
-import BackButton from "@/src/components/elements/back";
-import { AuthForm } from "@/src/components/features/forms/auth-form";
-import { getSession } from "@/src/lib/auth";
-import { redirect } from "next/navigation";
+import BackButton from '@/src/components/elements/back';
+import { AuthForm } from '@/src/components/features/forms/auth-form';
+import { getSession } from '@/src/lib/auth';
+import { redirect } from 'next/navigation';
 
 export default async function Signin() {
   const session = await getSession();
   if (session) {
-    redirect("/journal");
+    redirect('/journal');
   }
   return (
     <div className="flex-1 font-sans grid grid-rows-[20px_1fr_20px] gap-16">
       <main className="flex flex-col gap-8 p-4">
-        <BackButton />
+        <div>
+          <BackButton />
+        </div>
         <div className="flex flex-col gap-8 items-center">
           <h1 className="font-serif text-3xl font-medium text-center">
             Enter your account to begin
